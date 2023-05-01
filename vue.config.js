876f1f133e37711e615fd0a7e6cf7c25aedf8560
LeoVeig4 <leoveiga2002@gmail.com>
2023-05-01 20:41:53 -0300

    docs vueconfig

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: "/DeD/",
+  publicPath: "/DeD-info/",
   transpileDependencies: true,
   lintOnSave: false,
 });
